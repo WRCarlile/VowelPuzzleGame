36f0b9f6945b5e3a0e31ae336bd2d4f72f51bbdb
Ryan Carlile <ryancarlile@gmail.com>
2016-04-22 10:46:59 -0700

    rework tests and pass the first test

--- src/main/java/VowelPuzzle.java
+++ src/main/java/VowelPuzzle.java
@@ -1,44 +1,39 @@
 import java.util.*;
 import java.util.Map;
 import java.util.HashMap;
-// import spark.ModelAndView;
-// import spark.template.velocity.VelocityTemplateEngine;
-// import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
 
 public class VowelPuzzle {
   public static void main(String[] args) {
-    // staticFileLocation("/public");
-    //   get("/", (request, response) -> {
-    //     Map<String, Object> model = new HashMap<String, Object>();
-    //     model.put("template", "templates/home.vtl");
-    //     return new ModelAndView(model, "templates/layout.vtl");
-    //   }, new VelocityTemplateEngine());
-    //
-    //   get("/detector", (request, response) -> {
-    //     Map<String, Object> model = new HashMap<String, Object>();
-    //
-    //     String userInput = request.queryParams("blank");
-    //     App newApp = new App();
-    //     Boolean results = newApp.methodName(userInput);
-    //     model.put("results", results);
-    //
-    //     model.put("template", "templates/detector.vtl");
-    //     return new ModelAndView(model, "templates/layout.vtl");
-    //   }, new VelocityTemplateEngine());
+    staticFileLocation("/public");
+      get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/home.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
+
+      get("/detector", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+
+        String userInput = request.queryParams("sentence");
+        VowelPuzzle newSentence = new VowelPuzzle();
+        String results = newSentence.vowelRemover(userInput);
+        model.put("results", results);
+
+        model.put("template", "templates/detector.vtl");
+        return new ModelAndView(model, "templates/layout.vtl");
+      }, new VelocityTemplateEngine());
   }
 
   public static String vowelRemover(String userInput) {
-    // String newSentence = userInput.toCharArray();
-
-    // char e = 'e';
     String newSentence = userInput.replace('e','-');
-    newSentence = newSentence.replace('a','-');
+    newSentence = newSentence.replace('t','-');
     newSentence = newSentence.replace('u','-');
     newSentence = newSentence.replace('i','-');
     newSentence = newSentence.replace('o','-');
     System.out.println(newSentence);
     return newSentence;
-
   }
-
 }
--- src/test/java/VowelPuzzleTest.java
+++ src/test/java/VowelPuzzleTest.java
@@ -3,10 +3,10 @@ import static org.junit.Assert.*;
 
 public class VowelPuzzleTest {
 
-  @Test
-public void vowelRemover_replaceeEwithDash_dashForE() {
+@Test
+public void vowelRemover_replaceSentenceVowelsWithDash_dashForVowels() {
   VowelPuzzle testVowelPuzzle = new VowelPuzzle();
-  String expected = "Th- sun will com- out tomorrow" ;
+  String expected = "Th- s-n w-ll c-m- --t t-m-rr-w" ;
   assertEquals(expected, testVowelPuzzle.vowelRemover("The sun will come out tomorrow"));
   }
 }
